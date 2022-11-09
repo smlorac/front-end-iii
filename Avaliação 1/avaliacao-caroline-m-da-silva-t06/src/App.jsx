a260021f6c5bcf6ea8b38aba5f56859de47fa3e6
Caroline Machado da Silva <ms.caroline@hotmail.com>
2022-11-08 22:25:15 -0300

    feat: avaliacao - adicionando validacoes

--- Avaliação 1/avaliacao-caroline-m-da-silva-t06/src/App.jsx
+++ Avaliação 1/avaliacao-caroline-m-da-silva-t06/src/App.jsx
@@ -4,11 +4,32 @@ import Card from "./components/Card";
 const App = () => {
   const [inputs, setInputs] = useState({ nome: "", casa: "" });
 
+  const [errors, setErrors] = useState([]);
+
+  let arrayErrors = [];
+
   const submit = (event) => {
     event.preventDefault();
-    console.log(inputs);
-    setInputs({...inputs, nome: "", casa: ""});
-    let isSubmitted = true;
+    //console.log(inputs);
+
+    if (inputs.nome.length < 3) {
+      arrayErrors.push("O nome deve ter, no mínimo, 3 caracteres");
+      setErrors(...errors, arrayErrors);
+    }
+
+    if (/\s/.test(inputs.nome)) {
+      arrayErrors.push("O nome não pode ter espaços em branco");
+      setErrors(...errors, arrayErrors);
+    }
+
+    if (inputs.casa.length < 6) {
+      arrayErrors.push("A casa deve ter, no mínimo, 6 caracteres");
+      setErrors(...errors, arrayErrors);
+    }
+
+    //console.log(errors);
+
+    setInputs({ ...inputs, nome: "", casa: "" });
   };
 
   return (
@@ -16,11 +37,11 @@ const App = () => {
       <h1>Cards do Harry Potter</h1>
       <form onSubmit={submit}>
         <label>Nome </label>
-        <input 
+        <input
           type="text"
           value={inputs.nome}
           onChange={(e) => {
-            setInputs({...inputs, nome: e.target.value});
+            setInputs({ ...inputs, nome: e.target.value });
           }}
         />
         <label>Casa de Hogwarts </label>
@@ -28,12 +49,15 @@ const App = () => {
           type="text"
           value={inputs.casa}
           onChange={(e) => {
-            setInputs({...inputs, casa: e.target.value});
+            setInputs({ ...inputs, casa: e.target.value });
           }}
         />
         <input type="submit" />
       </form>
-      <Card nome={inputs.nome} casa={inputs.casa}/>
+      <div>
+        {errors.map((error, index) => <p key={`error-list-${index}`}>{error}</p>)}
+      </div>
+      <Card nome={inputs.nome} casa={inputs.casa} />
     </>
   );
 };
