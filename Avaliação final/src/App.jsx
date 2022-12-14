d4221cfb1de23477df753f842cd855c1407cde20
Caroline Machado da Silva <ms.caroline@hotmail.com>
2022-12-14 19:54:20 -0300

    fix/avaliacao: dark theme on body

--- Avaliação final/src/App.jsx
+++ Avaliação final/src/App.jsx
@@ -1,17 +1,12 @@
-import { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-import { ThemeContext } from "./Providers/ThemeProvider";
-import "./index.css";
 
 function App() {
-  const { theme } = useContext(ThemeContext);
 
   return (
     <>
-      {/* FIXME body não fica escuro*/}
-      <div className={theme === "light" ? `light` : `dark`} data-testid="app">
+      <div data-testid="app">
         <Navbar />
         <main>
           <Outlet />
--- Avaliação final/src/Routes.jsx
+++ Avaliação final/src/Routes.jsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Routes/Home";
 import Login from "./Routes/Login";
@@ -5,18 +6,23 @@ import Detail from "./Routes/Detail";
 import Contato from "./Routes/Contato";
 import Destacados from "./Routes/Destacados";
 import NotFound from "./Routes/NotFound";
+import { ThemeContext } from "./Providers/ThemeProvider";
 
 const AppRoutes = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <Routes>
-      <Route exact path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/dentist/:matricula" element={<Detail />} />
-      <Route path="/contato" element={<Contato />} />
-      <Route path="/destacados" element={<Destacados />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div id="appRoutes" className={theme === "light" ? `light` : `dark`}>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dentist/:matricula" element={<Detail />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/destacados" element={<Destacados />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 };
 
